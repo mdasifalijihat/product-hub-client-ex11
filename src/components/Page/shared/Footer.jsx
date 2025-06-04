ba4001222cf4649450658f91e9d87f4de6a1e2e9
mdasifalijihat <mdasifalijihat@gmail.com>
2025-06-04 16:59:55 +0600

    add footer section

--- src/components/Page/shared/Footer.jsx
+++ src/components/Page/shared/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io";
+import { Link } from "react-router";
 
 const Footer = () => {
-    return (
-        <div>
-            foooter 
-        </div>
-    );
+  return (
+    <div>
+      <footer className="footer footer-horizontal footer-center bg-white p-10">
+        <aside>
+          
+          <p className="font-bold">
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+        </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4">           
+              <Link to={'/facebook.com'}> <FaFacebook size={24} /> </Link>
+              <Link to={'/instagram.com'} > <FaInstagram size={24}   /> </Link>
+              <Link to={'/linkedin.com'}> <IoLogoLinkedin size={24}  /> </Link>
+            
+          </div>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
